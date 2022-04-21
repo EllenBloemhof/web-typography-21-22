05ad50097558c6e1521c06fe838f5c6dd59d1498
EllenBloemhof <74147677+EllenBloemhof@users.noreply.github.com>
2022-04-21 20:32:50 +0200

    21-4

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,6 +1,7 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
+	0,
 	2.95,
 	7.3,
 	8.9,
@@ -12,9 +13,11 @@ sounds = [
 	23.5,
 	23.7,
 	25.8,
+	26,
 	31,
 	32.8,
 	33,
-	34,
-	86
+	42,
+	58,
+	75
 ];
